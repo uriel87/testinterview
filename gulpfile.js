cfe843ea96065c3a08b6090633e4b407c7eba46f
uriel87 <urielch87@gmail.com>
2018-01-04 21:01:49 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,29 +12,29 @@ var gulp = require('gulp'),
 gulp.task('browserSync', function() {
     browserSync({
         server: {
-            baseDir: './'
+            baseDir: 'src'
         },
     })
 });
 
 gulp.task('watch', ['browserSync'], function (){
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch('./**/*.html', browserSync.reload);
-    gulp.watch('./js/**/*.js', browserSync.reload);
-    gulp.watch('./css/**/*.css', browserSync.reload);
+    gulp.watch('src/**/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('src/css/**/*.css', browserSync.reload);
 });
 
 gulp.task('useref', function(){
-    return gulp.src('**/*.html')
+    return gulp.src('src/**/*.html')
         .pipe(useref())
-        .pipe(gulpIf('./js/**/*.js', uglify()))
+        .pipe(gulpIf('src/js/**/*.js', uglify()))
         // Minifies only if it's a CSS file
-        .pipe(gulpIf('./css/*.css', cssnano()))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulpIf('src/css/*.css', cssnano()))
+        .pipe(gulp.dest('bundle'))
 });
 
-gulp.task('clean:dist', function() {
-    return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+gulp.task('clean:bundle', function() {
+    return del.sync(['bundle/**/*', '!bundle/images', '!bundle/images/**/*']);
 });
 
 gulp.task('default', ['watch', 'useref']);
